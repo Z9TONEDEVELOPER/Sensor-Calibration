ecdcf249990b320193c3ceb7a68ce5a57168a6de
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2026-01-27 00:59:15 +0300

    add xaml design

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Controls;
 
 namespace CalibrationApp.Views;
 
@@ -7,5 +9,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    public void UploadData(object? sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog();
+        openFileDialog.Title = "Select Data File";
+        openFileDialog.Filters.Add(new FileDialogFilter() { Name = "Data Files", Extensions = { "csv", "txt" } });
+        openFileDialog.AllowMultiple = false;
+
+        var result = openFileDialog.ShowAsync(this);
     }
 }
